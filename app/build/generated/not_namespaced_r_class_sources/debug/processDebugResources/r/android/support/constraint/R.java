65a6112667f6802f98285417cf2cd8012bcd13f5
gloin666 <demariejanick@gmail.com>
2019-03-22 14:34:04 +0100

    added card for favorits xml file favroute_row added layout to fragmanent_favorite

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080027;
-        public static final int end = 0x7f080045;
-        public static final int gone = 0x7f080053;
-        public static final int invisible = 0x7f080060;
-        public static final int left = 0x7f080067;
-        public static final int packed = 0x7f080082;
-        public static final int parent = 0x7f080084;
-        public static final int percent = 0x7f080087;
-        public static final int right = 0x7f08008d;
-        public static final int spread = 0x7f0800b5;
-        public static final int spread_inside = 0x7f0800b6;
-        public static final int start = 0x7f0800bb;
-        public static final int top = 0x7f0800d3;
-        public static final int wrap = 0x7f0800e4;
+        public static final int bottom = 0x7f080028;
+        public static final int end = 0x7f080046;
+        public static final int gone = 0x7f080054;
+        public static final int invisible = 0x7f080062;
+        public static final int left = 0x7f08006a;
+        public static final int packed = 0x7f080085;
+        public static final int parent = 0x7f080087;
+        public static final int percent = 0x7f08008a;
+        public static final int right = 0x7f080090;
+        public static final int spread = 0x7f0800b9;
+        public static final int spread_inside = 0x7f0800ba;
+        public static final int start = 0x7f0800bf;
+        public static final int top = 0x7f0800d7;
+        public static final int wrap = 0x7f0800eb;
     }
     public static final class styleable {
         private styleable() {}
